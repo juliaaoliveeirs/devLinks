d56017e44ab02c4303f49844183cf71f96bac839
Julia Oliveira <julia.oliveira.silva@outlook.com>
2023-10-20 21:14:40 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle('light');
+
+  const img = document.querySelector('img');
+
+  if (html.classList.contains('light')) {
+    img.setAttribute('src', './assets/avatar-light.png');
+    img.setAttribute(
+      'alt',
+      'Foto de Julia Silva, sorrindo, cabelo grande e escuro, com blusa branca e rosa claro, com a cabeça para o lado esquerdo e com fundo claro.'
+    );
+  } else {
+    img.setAttribute('src', './assets/avatar.png');
+    img.setAttribute(
+      'alt',
+      'Foto de Julia Silva, sorrindo, cabelo grande e escuro, com blusa branca e rosa claro, com a cabeça para o lado direito e com fundo claro.'
+    );
+  }
+}
