b1c873780d285bdf2c1d356030b60a1dea1575b2
Julia Oliveira <31010583+juliaaoliveeirs@users.noreply.github.com>
2023-10-27 12:54:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function toggleMode() {
       'Foto de Julia Silva, sorrindo, cabelo grande e escuro, com blusa branca e rosa claro, com a cabeça para o lado esquerdo e com fundo claro.'
     );
   } else {
-    img.setAttribute('src', './assets/avatar.png');
+    img.setAttribute('src', './assets/Avatar.png');
     img.setAttribute(
       'alt',
       'Foto de Julia Silva, sorrindo, cabelo grande e escuro, com blusa branca e rosa claro, com a cabeça para o lado direito e com fundo claro.'
